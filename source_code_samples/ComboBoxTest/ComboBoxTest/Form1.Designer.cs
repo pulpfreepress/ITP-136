88032f0d71c838bd701c6542346af4408c375fd9
swodogrick <rick@pulpfreepress.com>
2022-03-03 10:54:06 -0500

    Modified .gitignore. Added ComboBoxText project.

---
+++ source_code_samples/ComboBoxTest/ComboBoxTest/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ComboBoxTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.websiteComboBox = new System.Windows.Forms.ComboBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.websiteComboBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.webBrowser1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(718, 319);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(353, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select a Website:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // websiteComboBox
+            // 
+            this.websiteComboBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.websiteComboBox.FormattingEnabled = true;
+            this.websiteComboBox.Location = new System.Drawing.Point(362, 3);
+            this.websiteComboBox.Name = "websiteComboBox";
+            this.websiteComboBox.Size = new System.Drawing.Size(353, 21);
+            this.websiteComboBox.TabIndex = 1;
+            // 
+            // webBrowser1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.webBrowser1, 2);
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(3, 33);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(712, 283);
+            this.webBrowser1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 319);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "ComboBoxTest";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox websiteComboBox;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ source_code_samples/ComboBoxTest/ComboBoxTest/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComboBoxTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeComboBox();
+
+        }
+
+
+        private void InitializeComboBox()
+        {
+            foreach(string s in Properties.Settings.Default.Websites)
+            {
+                websiteComboBox.Items.Add(s);
+            }
+
+            websiteComboBox.SelectedIndex = 0;
+            websiteComboBox.SelectedIndexChanged += ComboBoxIndexChangedEventHandler;
+
+        }
+
+
+
+        private void ComboBoxIndexChangedEventHandler(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(websiteComboBox.SelectedItem.ToString());
+        }
+
+
+
+
+
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
